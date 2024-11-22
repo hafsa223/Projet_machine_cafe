059c171a1499ca5bc059e0cb733f91e24e25f8ee
Enzo Sandre <enzo@enzosandre.fr>
2024-11-22 10:25:58 +0100

    [RED] MVP - Cas nominal

--- test/machine_a_cafe_test.py
+++ test/machine_a_cafe_test.py
@@ -5,6 +5,18 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
 
+    def test_cas_nominal(self):
+        # ETANT DONNE une machine a café
+        lecteur_cb = LecteurCbPourLesTests()
+        brewer = BrewerSurveillantLesAppels()
+        machine_a_cafe = MachineACafé()
+
+        # QUAND une carte est détectée
+        lecteur_cb.simuler_carte_détectée()
+
+        # ALORS un café est commandé au hardware
+        self.assertTrue(brewer.make_a_coffee_appelé())
+
 
 if __name__ == '__main__':
     unittest.main()

f89eea40d7ac31623398beaba1d06c0568b48162
Enzo Sandre <enzo@enzosandre.fr>
2024-11-22 10:45:45 +0100

    [GREEN]

--- src/machine_a_cafe.py
+++ src/machine_a_cafe.py
@@ -1,6 +1,11 @@
 from hardware.brewer import BrewerInterface
+from hardware.creditcard import CreditCardInterface, CardHandleInterface
 
 
 class MachineACafé:
-    def __init__(self, brewer: BrewerInterface):
-        brewer.make_a_coffee()
+    def __init__(self, brewer: BrewerInterface, lecteur_cb: CreditCardInterface):
+        lecteur_cb.register_card_detected_callback(self._credit_card_callback)
+        self._brewer = brewer
+
+    def _credit_card_callback(self, card_handle: CardHandleInterface) -> None:
+        self._brewer.make_a_coffee()
